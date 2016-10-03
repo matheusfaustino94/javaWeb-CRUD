24d2520042fae1ea9917e1d43984f5fe8011f477
Matheus Faustino <matheusfaustino94@live.com>
2016-10-03 17:03:36 -0300

    Subindo códigos iniciais

---
+++ cjweb1/src/br/com/m5Project/jdbc/Conexao.java
@@ -0,0 +1,25 @@
+package br.com.m5Project.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexao{
+	
+	public static Connection getConnection() {
+		Connection con = null;
+		String driver = "jdbc:postgresql://localhost:5432/bancocjweb1";
+		String user = "postgres";
+		String password = "mr1418";
+		try {
+			con = DriverManager.getConnection(driver, user, password);
+			System.out.println("Conexão com o banco: "+driver+" efetuada com sucesso");
+		} catch (SQLException e) {
+			System.out.println("Erro de conexão");
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+}
---
+++ cjweb1/src/br/com/m5Project/teste/Teste.java
@@ -0,0 +1,13 @@
+package br.com.m5Project.teste;
+
+import br.com.m5Project.jdbc.Conexao;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Conexao.getConnection();
+
+	}
+
+}
